3c20effb5a54ff10d7e9facf260f51bb8970cb86
Gil Toré <jjgilt@indra.es>
2025-05-26 13:46:49 +0200

    añadido sesiones al user y sus excepciones

--- src/main/java/com/playpass/backend/user/infraestructure/advice/UserExceptionHandler.java
+++ src/main/java/com/playpass/backend/user/infraestructure/advice/UserExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.playpass.backend.user.infraestructure.advice;
 
 import com.playpass.backend.shared.dto.ErrorDTO;
+import com.playpass.backend.user.domain.exception.UserNotAviableSesionException;
 import com.playpass.backend.user.domain.exception.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,13 @@ public class UserExceptionHandler {
         return  ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(new ErrorDTO("USER_NOT_FOUND",ex.getMessage()));
+    }
+
+    @ExceptionHandler(UserNotAviableSesionException.class)
+    public ResponseEntity<ErrorDTO> handleUserNotAviableSesionException(UserNotAviableSesionException ex) {
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(new ErrorDTO("USER_NOT_AVAILABLE_SESIONS",ex.getMessage()));
     }
 
 }
